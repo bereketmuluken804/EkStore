0ba391c941ee51fe0823940070356937140c9500
M. Bereket <bmuluken804@gmail.com>
2026-08-13 08:41:33 +0300

    setup clerk webhook to neon database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,26 @@
 import express from "express";
+// import open from "open";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { clerkWebhookHandler } from "./webhooks/clerk";
+import { getEnv } from "./lib/env";
 
 const app = express();
+const env = getEnv();
+const rawJson = express.raw({type: 'application/json', limit: "1mb"});
 
-app.listen(3001, () => {
-  console.log('listening port 3001');
-  
+// don't parse the webhook event data, it should be the raw format
+app.post("/webhook/clerk", rawJson,(req, res) => {
+  void clerkWebhookHandler(req, res);
+})
+
+app.use(express.json())
+app.use(cors())
+app.use(clerkMiddleware());
+
+
+app.listen(env.PORT, async () => {
+  console.log(`listening port http://localhost:${env.PORT}`);
+  // await open(`http://localhost:${env.PORT}`);
 })
